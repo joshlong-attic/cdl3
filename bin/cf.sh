68b8fbce9bd1407351386e2c6b0d9f371e142eb5
Josh Long <josh@joshlong.com>
2016-12-12 19:49:17 -0800

    incorporating Cloud Foundry into the build.

---
+++ bin/cf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CF_USER=${1:-$CF_USER}
+CF_PASSWORD=${2:-$CF_PASSWORD}
+CF_ORG=${3:-$CF_ORG}
+CF_SPACE=${4:-$CF_SPACE}
+
+
+
+function install_cf(){
+    mkdir -p $HOME/bin
+    curl -v -L -o cf.tgz 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.15.0&source=github-rel'
+    tar zxpf cf.tgz
+    mkdir -p $HOME/bin && mv cf $HOME/bin
+}
+
+function validate_cf(){
+
+    cf  -v || install_cf
+    export PATH=$PATH:$HOME/bin
+
+    cf api $CF_API
+    cf auth $CF_USER "$CF_PASSWORD" && cf target -o $CF_ORG -s $CF_SPACE &&  cf apps
+}
+
+validate_cf
+
+BP=https://github.com/cloudfoundry/java-buildpack.git
+
+cf push -b $BP -p target/*jar --hostname cdlive-staging cdlive
